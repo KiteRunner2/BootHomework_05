75f412ce4911ea7a98c3aa86ab2a710ff554b954
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-02-05 10:40:47 -0500

    building layout

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 var a = moment().format('dddd, MMMM Do');
 $( '#currentDay' ).text(a);
 console.log(a);
+
+$( '.btn' ).click(() => console.log('save btn clicked'));
+$( '.timeblock' ).click( (event) => console.log(event));
