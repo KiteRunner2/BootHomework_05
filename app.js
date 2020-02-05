5b815293b2fd5c54fc5e0c37300c94ffd91e0513
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-02-05 13:09:06 -0500

    adding logic and structure

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-var a = moment().format('dddd, MMMM Do');
-$( '#currentDay' ).text(a);
-console.log(a);
+const dayofWeek = moment().format('dddd, MMMM Do');
+$( '#currentDay' ).text(dayofWeek);
 
-$( '.btn' ).click(() => console.log('save btn clicked'));
-$( '.timeblock' ).click( (event) => console.log(event));
+const currentTime = moment().format('HH:MM');
+const currentHour = moment().format('HH');
+$( '#currentTime').text(currentTime);
+console.log(currentHour);
+
+function showText(id) {
+    //debugger;
+    let idt = 'id_' + id;
+    console.log(idt);
+    console.log($( `#${idt}`).val());
+}
+
+function greyOut(hour){
+    let a = document.getElementById('container');
+    let b = a.children;
+    let c = Array.from(b)
+    //console.log(c)
+    c.forEach(value => {
+        let d = value.children[1];
+        d.className = "timeblock-inactive";
+        console.log(d.value);
+    })
+    //a.childElementCount;
+    //console.log(c)
+}
+$( '.btn' ).click(( event) => showText(event.target.id));
+$( '.timeblock' ).click( (event) => console.log(event.target.id));
+
+greyOut();
