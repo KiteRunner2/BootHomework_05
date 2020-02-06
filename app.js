ce651ce9bb5c08d243a2be3a74075bca029faec1
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-02-05 21:35:36 -0500

    main logic completed
    load, save function ready

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ let init = (initF = () => {
 
     const weekDay = moment().format('dddd, MMMM Do');
     const timeCurrent = moment().format('HH:MM');
-    const hourCurrent = moment().format('HH');
+    //const hourCurrent = moment().format('HH');
+    const hourCurrent = 11;
     $( '#currentTime').text(timeCurrent);
     $( '#currentDay' ).text(weekDay);
 
+    /*
+    function storageInit(){
+        let data = ['text'];
+        localStorage.setItem('calendar',JSON.stringify(data));
+    }
+    */
+   Load();
+    //Save();
+    //storageInit();
     return {
         dayofWeek : weekDay,
         currentTime : timeCurrent,
@@ -15,29 +25,31 @@ let init = (initF = () => {
 
 })();
 
-
+/*
 function showText(id) {
     //debugger;
     let idt = 'id_' + id;
     console.log(idt);
     console.log($( `#${idt}`).val());
 }
+*/
 
-function greyOut(hour){
-    let a = document.getElementById('container');
-    let b = a.children;
-    let c = Array.from(b)
+
+function greyOut(){
+    let containerBox = document.getElementById('container');
+    let children = containerBox.children;
+    let childrenArray = Array.from(children)
     //console.log(c)
-    c.forEach(value => {
-        let d = value.children[1];
-        let tt = Number(d.id.slice(3));
-        console.log(tt,init.currentHour,tt<init.currentHour);
-        if (tt == Number(init.currentHour)) {
-            d.className = "timeblock-current";
-            d.placeholder = "What is your task?";
+    childrenArray.forEach(value => {
+        let timeblock = value.children[1];
+        let timeblockId = Number(timeblock.id.slice(3));
+        //console.log(tt,init.currentHour,tt<init.currentHour);
+        if (timeblockId == Number(init.currentHour)) {
+            timeblock.className = "timeblock-current";
+            timeblock.placeholder = "What is your task?";
         } 
         else {
-        tt < Number(init.currentHour) ? d.disabled=true:d.className = "timeblock";
+        timeblockId < Number(init.currentHour) ? timeblock.disabled=true:timeblock.className = "timeblock";
         }
         
         
@@ -47,19 +59,33 @@ function greyOut(hour){
     //console.log(c)
 }
 
-function aInit(){
-    let data = ['id','text'];
-    localStorage.setItem('calendar',JSON.stringify(data));
-}
+
 
 function Load(){
-
-
+    let inputs;
+    if (localStorage.getItem('calendar') != null){
+        inputs = JSON.parse(localStorage.getItem('calendar'));
+    }
+    else return;
+    //let inputs = JSON.parse(localStorage.getItem('calendar'));
+    let divs = Array.from(document.getElementById('container').children);
+    divs.forEach((value,key) => {
+        value.children[1].value = inputs[key];
+        //console.log(value,key);
+    })
+    //console.log(divs);
+    //console.log(inputs);
 }
 
 function Save(){
-
-
+    let inputValues = []
+    let divs = Array.from(document.getElementById('container').children);
+    divs.forEach(value => {
+        console.log(value.children[1].value);
+        inputValues.push(value.children[1].value);
+    });
+    console.log(inputValues);
+    localStorage.setItem('calendar',JSON.stringify(inputValues));
 }
 
 function Reset(){
@@ -68,7 +94,11 @@ function Reset(){
 }
 
 
-$( '.btn' ).click(( event) => showText(event.target.id));
-$( '.timeblock' ).click( (event) => console.log(event.target.id));
+$( '.btn' ).click(Save);
+//$( '.timeblock' ).click( (event) => console.log(event.target.id));
+$( '.timeblock').keyup((event) => {
+    if (event.keyCode == 13) Save();
+});
 
 greyOut();
+Load();
